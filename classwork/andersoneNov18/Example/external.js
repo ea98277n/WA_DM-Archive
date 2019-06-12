cada2a23f202a55532e0276527ba51f8c3ac7f46
Eric Anderson <ea98277n@pace.edu>
2019-06-12 19:10:59 -0400

    Add archive to repository

---
+++ classwork/andersoneNov18/Example/external.js
@@ -0,0 +1,9 @@
+function addNumbers() {
+    
+    var num1 = parseInt(prompt("Enter number 1","3"));
+    var num2 = parseInt(prompt("Enter number 3","67"));
+    
+    document.write("Number 1 is: " + num1 + "<br>");
+    document.write("Number 2 is: " + num2 + "<br>");
+    document.write("The total is: " + (num1+num2));
+}
